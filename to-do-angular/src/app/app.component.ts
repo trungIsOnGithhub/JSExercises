5069b793d25fb9bc9dda8fad9e551c0eddc1b8f4
trungvuive <trung150502@gmail.com>
2024-02-07 23:36:10 +0700

    update code into seperate folder

--- to-do-angular/src/app/app.component.ts
+++ to-do-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,53 +6,5 @@ import { Component, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  index = 1;
   title = 'My Todo List';
-  tasks = [
-    new Task('Mua Iphone 15'),
-    new Task('Mua xe Bentley'),
-    new Task('Gui 500tr cho Bo'),
-    new Task('Du Lich Chau Au'),
-    new Task('Nhan Offer $100K')
-  ];
-
-  @ViewChild('newTaskInput') newTaskInput : any;
-
-  ngOnInit() : void {}
-  getNewTaskInput() : string {
-    return this.newTaskInput.nativeElement.value;
-  }
-  clearNewTaskInput() : void {
-    this.newTaskInput.nativeElement.value = '';
-  }
-
-  addTask() : void {
-    // confirm(`Do you want to add task: ${newTaskName}`);
-
-    this.tasks.push(new Task(this.getNewTaskInput()));
-
-    this.clearNewTaskInput();
-  }
-
-  removeTask(taskNameToRemove : string) : void
-  {
-    // confirm(`Do you want to remove task: ${taskNameToRemove}`);
-
-    this.tasks = this.tasks.filter(element => element.name !== taskNameToRemove)
-  }
-
-  toggleTaskDoneStatus(taskToMark : Task) : void {
-    // alert(`Task ${taskToMark.name} is Done!`);
-
-    taskToMark.isDone = !taskToMark.isDone;
-  }
-}
-
-class Task {
-  public name : string;
-  public isDone : boolean = false;
-
-  constructor(taskName : string) {
-    this.name = taskName;
-  }
 }
