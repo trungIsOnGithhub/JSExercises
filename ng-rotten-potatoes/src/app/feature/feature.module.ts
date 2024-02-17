31b98f43850ae31656d300b8074269f95f1595c7
trungvuive <trung150502@gmail.com>
2024-02-17 11:53:54 +0700

    create new feature module

---
+++ ng-rotten-potatoes/src/app/feature/feature.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StartRatingComponent } from '../src/app/feature/start-rating/start-rating.component';
+
+@NgModule({
+  declarations: [
+  
+    StartRatingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class FeatureModule { }
